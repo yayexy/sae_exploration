fea94a2e81b04a85bd242acb73c9dd51f87a5408
whoselevenn <alois.pourny@gmail.com>
2025-03-12 22:13:31 +0100

    création d'une fonction de générateur d'instance

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <time.h>
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
 
 struct Ville
 {
@@ -14,15 +15,7 @@ float calculerDistance(const Ville& a, const Ville& b){
     return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
 
-int main(){
-    srand(time(NULL));
-
-    int tailleZone = 1000;
-
-    int nombreVilles;
-    std::cout << "Entrez le nombre de villes : ";
-    std::cin >> nombreVilles;
-
+void generateurInstance(const int& nombreVilles, const int& tailleZone){
     std::vector<Ville> villes(nombreVilles);
 
     // Génération aléatoire des coordonnées des villes
@@ -37,7 +30,7 @@ int main(){
     if (!fichier)
     {
         std::cerr << "Erreur lors de l'ouverture du fichier !" << std::endl;
-        return 1;
+        return;
     }
 
     fichier << nombreVilles << std::endl;
@@ -66,6 +59,18 @@ int main(){
 
     fichier.close();
     std::cout << "Instance générée et sauvegardée avec succès !" << std::endl;
+}
 
+int main(){
+    srand(time(NULL));
+
+    int tailleZone = 1000;
+
+    int nombreVilles;
+    std::cout << "Entrez le nombre de villes : ";
+    std::cin >> nombreVilles;
+
+    generateurInstance(nombreVilles, tailleZone);
+    
     return 0;
 }

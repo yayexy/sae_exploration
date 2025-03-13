fe7c884e434b7e1425587a1de7e3e2ca29140d7b
whoselevenn <alois.pourny@gmail.com>
2025-03-13 16:27:23 +0100

    Prim Alogrithm fait
    Prim Algorithm fait pour tout type de graphe avec un min-heap priority queue pour maximiser le temps

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <limits>
+
+#include <queue>
 
 struct City
 {
@@ -11,12 +14,18 @@ struct City
     int y;
 };
 
-// struct Edge
-// {
-//     int weight;
-//     int start;
-//     int destination;
-// };
+struct Edge
+{
+    int start;
+    int destination;
+    float weight;
+};
+
+struct Compare {
+    bool operator()(const Edge& e1, const Edge& e2){
+        return e1.weight > e2.weight;
+    }
+};
 
 float calculateDistance(const City& a, const City& b){
     return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
@@ -86,14 +95,43 @@ std::vector<std::vector<float>> generateInstance(const int& totalCities, const i
     return distanceMatrix;
 }
 
-std::vector<int> primAlgorithm(const int& totalCities, const std::vector<std::vector<float>>& distanceMatrix){
-    // stores the edges of the mst
-    std::vector<int> mst;
-    // stores if a node is included in the mst
-    std::vector<bool> inMST(totalCities, false);
+std::vector<Edge> primAlgorithm(const int& totalCities, const std::vector<std::vector<float>>& distanceMatrix) {
+    std::vector<Edge> mst; // Stores the edges of the Minimum Spanning Tree (MST)
+    std::vector<bool> inMST(totalCities, false); // Tracks whether a node is included in the MST
+    std::priority_queue<Edge, std::vector<Edge>, Compare> pq; // Priority queue (min-heap)
 
+    inMST[0] = true;
+    for (int i = 1; i < totalCities; i++)
+    {
+        pq.push({0, i, distanceMatrix[0][i]});
+    }
+    
+    while (!pq.empty() && mst.size() < totalCities - 1)
+    {
+        Edge edge = pq.top();
+        pq.pop();
+
+        int u = edge.destination;
+        if (inMST[u])
+        {
+            continue;
+        }
+        
+        inMST[u] = true;
+        mst.push_back(edge);
+
+        for (int v = 0; v < totalCities; v++)
+        {
+            if (!inMST[v])
+            {
+                pq.push({u, v, distanceMatrix[u][v]});
+            }
+        }
+    }
+    
     return mst;
 }
+
 
 int main(){
     srand(time(NULL));
@@ -106,7 +144,20 @@ int main(){
 
     std::vector<std::vector<float>> distanceMatrix = generateInstance(totalCities, areaSize);
     
-    std::vector<int> mst = primAlgorithm(totalCities, distanceMatrix);
+    // std::vector<std::vector<float>> distanceMatrix = {
+    //     {0, 757.341, 339.236, 735.218, 424.449},
+    //     {757.341, 0, 587.231, 365.308, 441.38},
+    //     {339.236, 587.231, 0, 431.335, 148.189},
+    //     {735.218, 365.308, 431.335, 0, 311.04},
+    //     {424.449, 441.38, 148.189, 311.04, 0}
+    // };
+
+    std::vector<Edge> mst = primAlgorithm(totalCities, distanceMatrix);
+    
+    for (const Edge& edge : mst)
+    {
+        std::cout << "De " << edge.start << " à " << edge.destination << " avec un poids de " << edge.weight << std::endl;
+    }
     
     return 0;
 }

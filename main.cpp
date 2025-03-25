0cd4a8327cf8a8468c34200bdeff6ae4655179d7
whoselevenn <alois.pourny@gmail.com>
2025-03-25 17:02:06 +0100

    Ajout de fonction
    Ajout de fonction print liste d'adjacence

--- main.cpp
+++ main.cpp
@@ -41,6 +41,45 @@ float calculateDistance(const Node& a, const Node& b){
     return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
 
+void printEdges(const std::vector<Edge>& mst){
+    std::cout << "\nAffichage des edges : " << std::endl;
+    for (const Edge& edge : mst)
+    {
+        std::cout << "De " << edge.start->number << " à " << edge.destination->number << " avec un poids de " << edge.weight << std::endl;
+    }
+}
+
+void printOddDegreeNodes(std::vector<Edge>& mst, std::vector<Node*>& odds){
+    std::cout << "\nfindOddsDegree: " << std::endl;
+    for (const Node* odd : odds)
+    {
+        std::cout << "Le degre du noeud " << odd->number << " est " << odd->degree << std::endl;
+        std::cout << "Coordinates : (" << odd->x << ", " << odd->y << ")" << std::endl;
+    }
+}
+
+void printNodesDegree(std::vector<Edge>& mst){
+    std::cout << "\nAffichage des degres : " << std::endl;
+    for (const Edge& edge : mst)
+    {
+        std::cout << "Node " << edge.start->number << ": degre " << edge.start->degree << std::endl;
+        std::cout << "Node " << edge.destination->number << ": degre " << edge.destination->degree << std::endl;
+    }
+}
+
+void printAdjacencyList(std::unordered_map<int, std::vector<int>> adjList){
+    std::cout << "\nListe d'adjacence : " << std::endl;
+    for (int i = 0; i < adjList.size(); i++)
+    {
+        std::cout << "Noeud " << i << " : [";
+        for (int j = 0; j < adjList[i].size(); j++)
+        {
+            std::cout << adjList[i][j] << ", ";
+        }
+        std::cout << "]" << std::endl;
+    }
+}
+
 std::vector<std::vector<float>> generateInstance(int& totalCities, const int& areaSize, const int& choice, std::vector<Node>& cities){
     std::vector<std::vector<float>> distanceMatrix;
 
@@ -276,16 +315,7 @@ std::vector<int> findEulerianCircuit(const std::vector<Edge>& mst, const int& to
         adjList[edge.destination->number].push_back(edge.start->number);
     }
     
-    std::cout << "\nListe d'adjacence : " << std::endl;
-    for (int i = 0; i < adjList.size(); i++)
-    {
-        std::cout << "Noeud " << i << " : [";
-        for (int j = 0; j < adjList[i].size(); j++)
-        {
-            std::cout << adjList[i][j] << ", ";
-        }
-        std::cout << "]" << std::endl;
-    }
+    printAdjacencyList(adjList);
 
     return circuit;
 }
@@ -340,32 +370,6 @@ std::vector<int> findEulerianCircuit(const std::vector<Edge>& mst, const int& to
 
 // }
 
-void printEdges(const std::vector<Edge>& mst){
-    std::cout << "\nAffichage des edges : " << std::endl;
-    for (const Edge& edge : mst)
-    {
-        std::cout << "De " << edge.start->number << " à " << edge.destination->number << " avec un poids de " << edge.weight << std::endl;
-    }
-}
-
-void printOddDegreeNodes(std::vector<Edge>& mst, std::vector<Node*>& odds){
-    std::cout << "\nfindOddsDegree: " << std::endl;
-    for (const Node* odd : odds)
-    {
-        std::cout << "Le degre du noeud " << odd->number << " est " << odd->degree << std::endl;
-        std::cout << "Coordinates : (" << odd->x << ", " << odd->y << ")" << std::endl;
-    }
-}
-
-void printNodesDegree(std::vector<Edge>& mst){
-    std::cout << "\nAffichage des degres : " << std::endl;
-    for (const Edge& edge : mst)
-    {
-        std::cout << "Node " << edge.start->number << ": degre " << edge.start->degree << std::endl;
-        std::cout << "Node " << edge.destination->number << ": degre " << edge.destination->degree << std::endl;
-    }
-}
-
 int main() {
     srand(time(NULL));
 
@@ -405,6 +409,7 @@ int main() {
     printNodesDegree(mst);
 
     // --- CALCUL DU CIRCUIT EULÉRIEN ---
+    std::cout << "\n=== Circuit Eulerien ===\n";
     circuit = findEulerianCircuit(mst, totalCities);
 
     return 0;
